a069525f8d7a56fadb86ddcc374d63bf961be6ad
George Kalandadze <kalandadzeg777@gmail.com>
2024-03-19 23:42:53 +0400

    feat/make models and migrations

---
+++ database/migrations/2024_03_19_192450_create_games_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Fixture::class)->cascadeOnDelete();
+            $table->unsignedInteger('home_team_goals')->default(0);
+            $table->unsignedInteger('away_team_goals')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
