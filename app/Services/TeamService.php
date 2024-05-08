3ed5fe134173e82327d4ce38d1ddf52b2ab41495
George Kalandadze <kalandadzeg777@gmail.com>
2024-05-08 15:33:28 +0400

    refactor/make logic that in each league is impossible to add more then 10 team

--- app/Services/TeamService.php
+++ app/Services/TeamService.php
@@ -3,11 +3,15 @@
 namespace App\Services;
 
 use App\Models\Team;
+use App\Repositories\Contracts\DivisionRepositoryContract;
 use App\Repositories\Contracts\TeamRepositoryContract;
 
 class TeamService
 {
-    public function __construct(protected TeamRepositoryContract $teamRepository)
+    public function __construct(
+        protected TeamRepositoryContract $teamRepository,
+        protected DivisionRepositoryContract $divisionRepository
+    )
     {
 
     }
@@ -17,15 +21,37 @@ class TeamService
         return $this->teamRepository->getAll();
     }
 
-    public function create(array $data): Team
+    public function create(array $data): Team|string
     {
+        $divisionId = $data['division_id'] ?? null;
+        if ($divisionId) {
+            $teamsCountInDivision = $this->countTeamsInDivision($divisionId);
+            if ($teamsCountInDivision >= 10) {
+                return 'Division has reached the maximum limit of teams.';
+            }
+        }
+
         return $this->teamRepository->create($data);
     }
 
-    public function update(int $id, array $data): Team
+    public function update(int $id, array $data): Team|string|null
     {
+        $team = $this->teamRepository->getById($id);
+        if (!$team) {
+            return null;
+        }
+
+        $newDivisionId = $data['division_id'] ?? $team->division_id;
+        if ($newDivisionId != $team->division_id) {
+            $teamsCountInNewDivision = $this->countTeamsInDivision($newDivisionId);
+            if ($teamsCountInNewDivision >= 10) {
+                return 'Division has reached the maximum limit of teams.';
+            }
+        }
+
         return $this->teamRepository->update($id, $data);
     }
+
 
     public function delete(int $id): bool
     {
@@ -35,5 +61,11 @@ class TeamService
     public function getById(int $id): ?Team
     {
         return $this->teamRepository->getById($id);
+    }
+
+    protected function countTeamsInDivision(int $divisionId): int
+    {
+        $division = $this->divisionRepository->getById($divisionId);
+        return $division->teams()->count();
     }
 }
